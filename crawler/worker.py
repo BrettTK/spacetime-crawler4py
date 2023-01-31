b9827ae483aa26793f14dd47ac70cb059fe97ae7
sophitt2 <123864962+sophitt2@users.noreply.github.com>
2023-01-31 15:55:07 -0800

    Update worker.py

--- crawler/worker.py
+++ crawler/worker.py
@@ -19,6 +19,7 @@ class Worker(Thread):
         
     def run(self):
         while True:
+            wordFrequency = {}
             tbd_url = self.frontier.get_tbd_url()
             if not tbd_url:
                 self.logger.info("Frontier is empty. Stopping Crawler.")
@@ -27,7 +28,7 @@ class Worker(Thread):
             self.logger.info(
                 f"Downloaded {tbd_url}, status <{resp.status}>, "
                 f"using cache {self.config.cache_server}.")
-            scraped_urls = scraper.scraper(tbd_url, resp)
+            scraped_urls = scraper.scraper(tbd_url, resp, wordFrequency)
             for scraped_url in scraped_urls:
                 self.frontier.add_url(scraped_url)
             self.frontier.mark_url_complete(tbd_url)
