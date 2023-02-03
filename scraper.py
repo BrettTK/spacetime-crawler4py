48b0d62a938cdc6bf86eaae5e781a15cff85c4ba
Courtesi <c.chang673@gmail.com>
2023-02-02 18:00:56 -0800

    ryan's version on 2/2

--- scraper.py
+++ scraper.py
@@ -1,8 +1,12 @@
 import re
 from urllib.parse import urlparse, urlsplit
+from bs4 import BeautifulSoup
+from collections import defaultdict
+
 
 #curtis test git push
-#ryan test push non-git but through github browser
+#ryan testing git push
+
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
@@ -51,3 +55,69 @@ def is_valid(url):
     except TypeError:
         print ("TypeError for ", parsed)
         raise
+
+"""
+takes in response.raw_response.content 
+
+- should be ignoring HTML markup 
+- should be putting words into a default dict with their frequencies
+- should be using a regex pattern to detect links 
+
+QUESTION: what to do with the dictionary of frequencies of each word
+
+return a list of the URLS 
+"""
+def tokenize(rrc, freqDict): #rrc stands for response.raw_response.content
+    
+#UNCERTAIN: should i generate individual dictionaries for every tokenize() call and not have freqDict as a parameter
+#OR update the dictionary of frequencies in the worker class
+    
+    stopWords = {
+    "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at",
+    "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could",
+    "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for",
+    "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's",
+    "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm",
+    "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't",
+    "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours",
+    "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't",
+    "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there",
+    "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too",
+    "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't",
+    "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's",
+    "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself",
+    "yourselves"
+    }
+    
+    #instantiate a defaultdict to hold the frequencies of all the words found
+    #frequencies = defaultdict(int)
+    
+    #instantiate a BeautifulSoup object to allow manipulation and extraction of data from the webpage
+    htmlContent = BeautifulSoup(rrc, 'lxml') #lxml is the most efficient and versatile parser and more effective than the standard 
+    URLS = [url.get('href') for url in htmlContent.find_all('a')]
+
+    #actual tokenizing starts here; 
+    
+    # Filter punctuation using regex with the exception of ' and - 
+    text = re.sub(r'[^\w\'-]+', ' ', htmlContent.get_text())
+    
+    # Split words joined by "-"
+    text = text.replace("-", " ")
+    
+    # Lowercase all words so we can accurately count all occurences of a word
+    text = text.lower()
+    
+    # Split text into words while filtering out words that are considered English stop words
+    words = [word for word in text.split() if word not in stopWords]
+    
+    # Count word occurrences
+    for word in words:
+        freqDict[word] += 1
+    
+    #REMEMBER: do something with the frequencies dict to count the overall frequencies from ALL the webpages
+    
+    #returns both the list of URLS and dictionary of frequencies as a tuple for later access
+    #temporary return values (not sure what to return)
+    #return URLS, frequencies
+    return URLS
+    
